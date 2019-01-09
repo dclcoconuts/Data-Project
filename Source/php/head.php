1d16c410db5c6d45a974e333638d34e7fdacc289
dclonyx <dcl.fabieno@18pixel.fr>
2019-01-09 14:47:22 +0100

    architecture

---
+++ Source/php/head.php
@@ -0,0 +1,6 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script src="./Source/js/jquery.vmap.js" type="text/javascript"></script>
+<script src="./Source/js/jquery.vmap.france.js" type="text/javascript"></script>
+<script src="./Source/js/jquery.vmap.colorsFrance.js" type="text/javascript"></script>
+<script src="./Source/js/jquery.creation.map.js" type="text/javascript"></script>
+<link href="./Source/css/jqvmap.css" media="screen" rel="stylesheet" type="text/css" />
