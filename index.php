5fb6a119351c1ea38779f91ce11079fb97dea068
dclonyx <dcl.fabieno@18pixel.fr>
2019-01-09 15:25:06 +0100

    test

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Map France</title> 
+    
+    <?php
+    include ('./Source/php/head.php');
+    ?>
+	
+</head>
+  <body>
+	<header>
+		<h1>Photos des monuments Historiques de France (1850 a 1914)</h1>
+	</header>
+	<div id="carte"> 
+		<div id="francemap" style="width: 1000px; height: 800px;"></div>
+	</div>
+  </body>
+</html>
