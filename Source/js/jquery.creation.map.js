52ff2b1a47dc4098b4a041dcfd7a65d38f742607
dclonyx <dcl.fabieno@18pixel.fr>
2019-01-14 10:12:55 +0100

    AMJ couleur carte

--- Source/js/jquery.creation.map.js
+++ Source/js/jquery.creation.map.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         showTooltip: true,
         onRegionClick: function(element, code, region)
         {
-            window.location.href = '/france_map.html?dep=' + code;
+            window.location.href = './Source/php/departement.php?dep=' + code;
         } 
     });
 });
