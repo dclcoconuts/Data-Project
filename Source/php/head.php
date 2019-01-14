e8dbf68d250333fdca4d201b8391a27559e2e7d5
dclonyx <dcl.fabieno@18pixel.fr>
2019-01-14 14:25:58 +0100

    ajout groupe marker

--- Source/php/head.php
+++ Source/php/head.php
@@ -1,9 +1,7 @@
+<link href="./Source/css/style.css" media="screen" rel="stylesheet" type="text/css" />
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 <script src="./Source/js/jquery.vmap.js" type="text/javascript"></script>
 <script src="./Source/js/jquery.vmap.france.js" type="text/javascript"></script>
 <script src="./Source/js/jquery.vmap.colorsFrance.js" type="text/javascript"></script>
 <script src="./Source/js/jquery.creation.map.js" type="text/javascript"></script>
-<link href="./Source/css/style.css" media="screen" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css" />
-<link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css" />
-<script type='text/javascript' src='https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js'></script>     
+  
